3016179db4c1e6337748d0194482be33f32a3869
e-izdfr <emad.izadifar@gmail.com>
2024-06-01 20:13:58 +0330

    Fixed a typo

--- tgbn-trade/exponential_moving_average.py
+++ tgbn-trade/exponential_moving_average.py
@@ -13,7 +13,7 @@ from torch_geometric.loader import TemporalDataLoader
 
 # local imports
 from tgb.nodeproppred.dataset_pyg import PyGNodePropPredDataset
-from modules.heuristics import ExponentialMovingAverage
+from heuristics import ExponentialMovingAverage
 from tgb.nodeproppred.evaluate import Evaluator
 
 
